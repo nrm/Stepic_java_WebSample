c7d090cb403ef59f1a635122d37a6f837e125086
Bezrukov Ilya <bezrukov@ipa.nw.ru>
2016-01-27 17:06:05 +0300

    Initial commit from IDEA

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by test on 27.01.16.
+ */
+
+import org.eclipse.jetty.server.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080);
+        ServletContextHandler
+
+    }
+}
